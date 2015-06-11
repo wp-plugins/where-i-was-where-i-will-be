3b0b8578cde7c91e691eecd425ca01bda45fea0c
mcnardelli <mcnardelli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-11 13:12:21 +0000

    Add title on local
    git-svn-id: https://plugins.svn.wordpress.org/where-i-was-where-i-will-be/tags/1.0.7@1178956 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ system/model/model_create.php
@@ -0,0 +1,65 @@
+<?php
+class WIWWIWB_Model_Create {
+    
+    private $table_prefix;
+    private $db;
+    function __construct() {
+        require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+        require_once(dirname(__FILE__).'/../include/include_admin.php');
+        $this->table_prefix = WIW_TABLE_PREFIX ;
+        $this->db = $wpdb;
+    }
+    
+/*****************************************************/
+//  ACTIVATE - CREATE TABLES
+/*****************************************************/
+
+    function activate_create_locals_table () {		
+		$table_name = $this->table_prefix . "locals";
+		$charset_collate = $this->db->get_charset_collate();
+		$sql = "CREATE TABLE {$table_name} (
+										id INT NOT NULL AUTO_INCREMENT,
+										title TEXT NULL,
+										city VARCHAR(256) NULL,
+										country VARCHAR(256) NOT NULL,
+                                        flag VARCHAR(8) NULL,
+										latitude REAL NOT NULL,
+										longitude REAL NOT NULL,
+										arrival DATE NULL,
+										departure DATE NULL,
+										url VARCHAR(1024) NULL,
+										image VARCHAR(1024) NULL,
+										type INT NOT NULL,
+										text TEXT NULL,
+										UNIQUE KEY id (id)
+				) {$charset_collate};";
+		dbDelta($sql);
+        
+    }
+    
+    function activate_create_type_table () {
+		$table_name = $this->table_prefix . "type";
+		$sql = "CREATE TABLE IF NOT EXISTS {$table_name} (id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+										name VARCHAR(256) NOT NULL,
+										pin VARCHAR(1024) NOT NULL);";
+		dbDelta($sql);
+        $this->populate_type();
+    }
+    
+    function drop_table($table) { 
+		$table_name = $this->table_prefix . $table;
+		$sql = "DROP TABLE {$table_name};";
+		$query = $this->db->query($sql);
+    }
+    
+    function populate_type() {
+        //Insert Undefined
+        return $this->db->query('INSERT IGNORE INTO '.$this->table_prefix.'type (id, name, pin) VALUES (1, "'.__('Undefined',WIW_TRANSLATE).'", "'.WIW_DIR_IMAGES.'pin/red-dot.png");');
+    }
+/******************************************/
+//  END ACTIVATE - CREATE TABLES
+/******************************************/
+
+
+}
+?>
